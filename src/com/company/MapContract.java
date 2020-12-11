8f468b0e0ea0b2a047ab932d40ce7b0d5e3c4cc4
olga <pakhomovaolga96@gmail.com>
2020-12-11 18:25:40 +0100

    - removed generic type from interface declaration

--- src/com/company/MapContract.java
+++ src/com/company/MapContract.java
@@ -3,8 +3,8 @@ package com.company;
 import java.util.List;
 import java.util.Map;
 
-interface MapContract<T> {
-    Map<String, List<T>> initializeCodeCityMap(String[] csvRow);
+interface MapContract {
+    Map<String, List<String>> initializeCodeCityMap(String[] csvRow);
 
     void updateListOfCities(String zipCode, String currentCityName);
 
